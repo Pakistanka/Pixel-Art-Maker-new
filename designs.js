c324bcf159af15ef7d2157967be51a14e72f96ac
zedfoxus <contact@zedfox.us>
2017-11-30 23:19:16 -0600

    Move code to makeGrid function

--- designs.js
+++ designs.js
@@ -1,26 +1,21 @@
 // Select color input
 let colorPicker = $('#colorPicker');
- 
-let submitButton = $('#submit').on('click', function makeGrid() {
+
+function makeGrid(){
+    // clear the table before drawing rows and columns
+    let drawTable = $('#pixel_canvas');
+    drawTable.empty();
   
-  // clear the table before drawing rows and columns
-  let drawTable = $('#pixel_canvas');
-  drawTable.empty();
-
-  // draw rows and columns
-  for(let i = 0; i < $('#input_height').val(); i++){
-    createRow = '<tr>';
-    for(let j = 0; j < $('#input_width').val(); j++){
-      createRow += '<td>&nbsp;</td>';
+    // draw rows and columns
+    for(let i = 0; i < $('#input_height').val(); i++){
+      createRow = '<tr>';
+      for(let j = 0; j < $('#input_width').val(); j++){
+        createRow += '<td>&nbsp;</td>';
+      }
+      createRow += '</tr>';
+      drawTable.append(createRow);
     }
-    createRow += '</tr>';
-    drawTable.append(createRow);
-  }
-
-})
-
-
-//function makeGrid()
+}
 
 // When size is submitted by the user, call makeGrid()
 
